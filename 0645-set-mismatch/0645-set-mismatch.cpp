b7bac789e706e12df48be38ddf0a49988091678c
Neeraj soni <113656442+0001nsoni@users.noreply.github.com>
2026-09-17 16:23:43 +0530

    Time: 0 ms (100%), Space: 26.3 MB (47.35%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> count(n + 1, 0);
+
+        for (int x : nums) {
+            count[x]++;
+        }
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 2)
+                duplicate = i;
+
+            if (count[i] == 0)
+                missing = i;
+        }
+
+        return {duplicate, missing};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
